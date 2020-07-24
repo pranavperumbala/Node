baa5b08620331aaec27859dd9ef57cf334187e11
pranavm <pranavm.perumbala@cet.ac.in>
2020-07-25 00:58:03 +0530

    Customized App

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,8 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=004acf4a3fc441b96a908b3e0eb32333&query=kasaragod#'
+const url = 'http://api.weatherstack.com/current?access_key=004acf4a3fc441b96a908b3e0eb32333&query=kasaragod&units=f'
 
 request({url:url, json: true} , (error, response) => {
-    console.log(response.body.request)
+    console.log(response.body.current.weather_descriptions[0],'.It is currently', response.body.current.temperature,'degrees out.It feels like',
+    response.body.current.feelslike,'degrees out')
 })
