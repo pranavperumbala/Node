252ccdac66abc8bc215d530221d10388edaf9bac
pranavm <pranavm.perumbala@cet.ac.in>
2020-07-25 18:30:17 +0530

    Setup next api request

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,3 +6,13 @@ request({url:url, json: true} , (error, response) => {
     console.log(response.body.current.weather_descriptions[0],'.It is currently', response.body.current.temperature,'degrees out.It feels like',
     response.body.current.feelslike,'degrees out')
 })
+
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicHJhbmF2bXVuZ2F0aCIsImEiOiJja2QxbHhsanIxNTAwMnVsNzhmZmR3cW4xIn0.-N7437oK_ECe3lzVNhjWgQ'
+
+request({ url :geocodeURL, json: true}, (error,response) => {
+    const latitude = response.body.features[0].center[0]
+    const longitude = response.body.features[0].center[1]
+    console.log(latitude)
+    console.log
+
+})
