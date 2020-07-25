eee3fffd76e4e1a3b23281efad8792fb580a7b3e
pranavm <pranavm.perumbala@cet.ac.in>
2020-07-26 00:37:39 +0530

    Added callback for mapbox request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,35 +1,45 @@
-const request = require('request')
+// const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=004acf4a3fc441b96a908b3e0eb32333&query=kasaragod&units=f'
+const request = require("request")
 
-request({url:url, json: true} , (error, response) => {
+const geocode = require('./utils/geocode.js')
 
-    if(error){
-        console.log(error)
-    }else if (response.body.error){
-        console.log('Unable to find location')
-    }
-    else{
-        console.log(response.body.current.weather_descriptions[0],'.It is currently', response.body.current.temperature,'degrees out.It feels like',
-        response.body.current.feelslike,'degrees out')
-    }
+// const url = 'http://api.weatherstack.com/current?access_key=004acf4a3fc441b96a908b3e0eb32333&query=kasaragod&units=f'
+
+// request({url:url, json: true} , (error, response) => {
+
+//     if(error){
+//         console.log(error)
+//     }else if (response.body.error){
+//         console.log('Unable to find location')
+//     }
+//     else{
+//         console.log(response.body.current.weather_descriptions[0],'.It is currently', response.body.current.temperature,'degrees out.It feels like',
+//         response.body.current.feelslike,'degrees out')
+//     }
    
-})
+// })
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/kasaragod.json?access_token=pk.eyJ1IjoicHJhbmF2bXVuZ2F0aCIsImEiOiJja2QxbHhsanIxNTAwMnVsNzhmZmR3cW4xIn0.-N7437oK_ECe3lzVNhjWgQ'
+// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/kasaragod.json?access_token=pk.eyJ1IjoicHJhbmF2bXVuZ2F0aCIsImEiOiJja2QxbHhsanIxNTAwMnVsNzhmZmR3cW4xIn0.-N7437oK_ECe3lzVNhjWgQ'
 
-request({ url :geocodeURL, json: true}, (error,response) => {
+// request({ url :geocodeURL, json: true}, (error,response) => {
 
-    if(error){
-        console.log(error)
-    }else if(response.body.features.length===0){
-        console.log('Unable to find location')
-    }else{
-        const latitude = response.body.features[0].center[0]
-        const longitude = response.body.features[0].center[1]
-        console.log(latitude)
-        console.log(longitude)
-    }
+//     if(error){
+//         console.log(error)
+//     }else if(response.body.features.length===0){
+//         console.log('Unable to find location')
+//     }else{
+//         const latitude = response.body.features[0].center[0]
+//         const longitude = response.body.features[0].center[1]
+//         console.log(latitude)
+//         console.log(longitude)
+//     }
     
 
+// })
+
+
+geocode('kasaragod', (error,data)=>{
+    console.log(error)
+    console.log(data)
 })
---
+++ weather-app/utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) +'.json?access_token=pk.eyJ1IjoicHJhbmF2bXVuZ2F0aCIsImEiOiJja2QxbHhsanIxNTAwMnVsNzhmZmR3cW4xIn0.-N7437oK_ECe3lzVNhjWgQ'
+    
+    request({url:url, json:true}, (error, response) => {
+        if(error){
+            callback('Unabele to connect to location services', undefined)
+        }else if(response.body.features.length===0){
+            callback('Unable to find location.Try another search', undefined)
+        }else{
+            callback(undefined, {
+                latitude:response.body.features[0].center[0],
+                longitude:response.body.features[0].center[1],
+                location:response.body.features[0].place_name
+            })
+        }
+    })
+
+
+}
+
+module.exports = geocode
