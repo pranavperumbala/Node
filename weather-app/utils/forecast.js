88705f1d2b217f89ce23bd09ebee2f414da318f3
pranavm <pranavm.perumbala@cet.ac.in>
2020-07-26 14:01:19 +0530

    Added Destructuring and shorthand property

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -3,14 +3,14 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=004acf4a3fc441b96a908b3e0eb32333&query=' + latitude + ',' + longitude +'&units=m'
     
-    request({url:url, json:true}, (error, response) => {
+    request({url, json:true}, (error, {body}) => {
         if(error){
             callback('Unable to connect to weather serviecs', undefined)
-        }else if(response.body.error){
+        }else if(body.error){
             callback('Unable to find location.Try another search', undefined)
         }else{
-            callback(undefined, response.body.current.weather_descriptions[0]+'.It is currently '+response.body.current.temperature+' degrees out.It feels like '+
-                   response.body.current.feelslike+' degrees out')
+            callback(undefined, body.current.weather_descriptions[0]+'.It is currently '+body.current.temperature+' degrees out.It feels like '+
+                   body.current.feelslike+' degrees out')
         }
     })
 }
