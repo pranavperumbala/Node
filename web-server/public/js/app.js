57f7b9cd77a992b3a9e8d880d182b9d33f7d8168
pranavm <pranavm.perumbala@cet.ac.in>
2020-07-30 00:05:46 +0530

    Browser request using fetch api

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
-console.log('asdjkafdgqwdv')
+
+
+fetch('http://localhost:3000/weather?address=kasaragod').then((response) => {
+    response.json().then((data) => {
+        if(data.error){
+            console.log(data.error)
+        }else{
+            console.log(data.forecast)
+            console.log(data.location)
+        }
+    })
+})
